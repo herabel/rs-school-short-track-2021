a0d8e13b7aaae00b226ab9ff3510220dcc68d8fb
herabel <3423.0a@mail.ru>
2021-04-12 23:06:38 +0300

    feat: do 4 more tasks

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = [...str];
+  const result = [1];
+  let i = 0;
+  while (i < arr.length) {
+    result.push(arr[i]);
+    const counter = result.length - 1;
+    while (arr[i] === arr[i + 1]) {
+      result[counter - 1] += 1;
+      i++;
+    }
+    result.push(1);
+    i++;
+  }
+  return result.filter((x) => x !== 1).join('');
 }
 
 module.exports = encodeLine;
